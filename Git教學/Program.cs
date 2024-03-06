50e687e990d298345dfddb776c903617c7c0fcc9
周政輝 <leo604leo604@gmail.com>
2024-03-06 18:16:19 +0800

    新增呼叫註冊的程式碼

--- Git教學/Program.cs
+++ Git教學/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Git教學.Service;
+
+Console.WriteLine("請輸入註冊帳號：");
+string account = Console.ReadLine();
+Console.WriteLine("請輸入註冊密碼：");
+string password = Console.ReadLine();
+Console.WriteLine("請輸入註冊姓名：");
+string name = Console.ReadLine();
+
+RegisterService service = new RegisterService();
+bool isSuccess = service.Register(account, password, name);
+if(isSuccess)
+    Console.WriteLine("註冊成功！");
+else 
+    Console.WriteLine("註冊失敗！");
